f9095f0fbb15f834822979f88269ec031243a6b4
olgamorozova1 <olgamorozova1>
2021-12-08 19:51:58 +0300

    Task1

---
+++ Tasks/Task1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    internal class Task1
+    {
+        public static void Main()
+        {
+            int[] array = new int[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i * 2;
+            }
+
+            foreach (int value in array)
+            {
+                Console.WriteLine(value);
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            int j = 0;
+
+            while (j < array.Length)
+            {
+                Console.WriteLine(array[j]);
+                j++;
+            }
+        }
+    }
+}

fc4df99703ef5e74b59c608e1d47b3acb1ec238a
olgamorozova1 <olgamorozova1>
2021-12-13 15:03:02 +0300

    Task2

---
+++ Tasks/Task2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    internal class Task2
+    {
+        public static void Main()
+        {
+            Point[] arrayOfPoints = new Point[10];
+
+            Point.FillArrayOfPointsWithRandomValues(arrayOfPoints);
+
+            Point.PrintArrayOfPoints(arrayOfPoints);
+
+            
+        }
+    }
+}
